c45e68df2169e388523c681295a1e87a9262e62b
emilio_watemberg <emilio.watemberg@gmail.com>
2016-11-12 13:19:35 -0300

    Modificacion preloader

--- js/custom.js
+++ js/custom.js
@@ -814,6 +814,7 @@ $(window).load(function() {
         $('#status').fadeOut(); // will first fade out the loading animation
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
         $('body').delay(350).css({'overflow':'visible'});
+
     }
 
 
